cccdcd21e02600412d29aa8c156b84ca8c5316f1
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-05-16 21:16:49 -0400

    this is my lab 01 part 2 programme

--- Lab_01/lab1b.py
+++ Lab_01/lab1b.py
@@ -5,9 +5,14 @@ Description : This is my first proramme
 
 '''
 print ("Welcome to PRG600!")
+#allow user to enter his/her name
 user_name = input("Enter your Name:")
+#allow user to enter the student ID
 user_id = input("Enter your Student ID:")
+#allow user to enter the age
 user_age = input("Enter your age:")
+#allow user to input favorite food
 user_food = input("Enter your favorite food:")
+#this statement is for display name,age,favorite food of the user
 print (user_name + " is " + user_age + " years old and loves to eat " + user_food + "!")
 print("Good luck with the course!")
