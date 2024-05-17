db650f89db18e4f931ab030a1d1d83144517c2e6
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-05-16 21:20:57 -0400

    This is my Lab 01 part 2- 1 question

--- Lab_01/lab2a.py
+++ Lab_01/lab2a.py
@@ -3,10 +3,14 @@ Name: Iresha Lakmali
 Student ID: 162279236
 
 '''
+#allow user to input two numbers
 user_num1 = input("Enter the first number: ")
 user_num2 = input("Enter the second number: ")
+
+#the string convert to a integer
 user_int1 = int(user_num1)
 user_int2 = int(user_num2)
+#now sum of entered numbers take to one variable called as result
 result = user_int1 + user_int2
 #print(result)
 print ("The result of " + str(user_int1)+" plus "+str(user_int2)+" is: " +str(result))
