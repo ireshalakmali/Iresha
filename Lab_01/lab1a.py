1b68f46c008758a36cab3daa3f9f9224360d2dd2
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-05-16 21:22:07 -0400

    did some changes in comments

--- Lab_01/lab1a.py
+++ Lab_01/lab1a.py
@@ -3,10 +3,11 @@ Name: Iresha Lakmali
 Student ID: 162279236
 Description : This is my first proramme
 '''
-#print a sentence
+#print a sentence Hello
 print ("Hello World")
 #allow user to enter a string
 user_name = input("Please enter your name : ")
+#print(hello+user_name)
 print ("Hello " + user_name + "!")
 
 
