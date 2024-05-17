aee2cf13758cad2c3a7d489d24e48973d4d5c68a
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-05-16 21:46:15 -0400

    This is my try for challenge python

--- Lab_01/challenge2.py
+++ Lab_01/challenge2.py
@@ -3,9 +3,10 @@ Name: Iresha Lakmali
 Student ID: 162279236
 
 '''
+#allow user to enter a decimal number between 0.0 to 0.9
 num1 = float(input("Enter a decimal number between 0.0 to 0.9: "))
 
-
+#round to the nearest integer and print it
 if (num1<0.5):
     num2 = int(num1)
     print (num2)
@@ -16,4 +17,12 @@ elif (num1>=0.5):
 
 else:
     print("error")
+
+#allow user to enter any number
+num = float(input("Enter a decimal number: "))
+
+#round to the nearest integer and print it
+number = int(num)
+print (number)
+
 
