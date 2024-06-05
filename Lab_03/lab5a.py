28d7036f1790842bf7894922638489d909977487
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-06-05 10:47:57 -0400

    second task of average done

--- Lab_03/lab5a.py
+++ Lab_03/lab5a.py
@@ -1,12 +1,16 @@
-user_numbers = []
-print("SUMMING CALCULATOR")
-
-while True:
- user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ")
- if user_input == "":
-    break
- else: 
-    user_numbers.append(int(user_input))
-
-print(user_numbers)
-print("Thank you for using summing calculator. The final sum was " + str(sum(user_numbers)) + ".")
+if __name__ == "__main__":
+ user_numbers = []
+ print("AVERAGE CALCULATOR")
+ while True:
+    user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ")
+    if user_input == "":
+        break
+    else: 
+        user_numbers.append(int(user_input))
+ 
+ num_sum = sum(user_numbers)
+ num_length = len(user_numbers)
+ average = num_sum / num_length
+ 
+ print(f"Total sum is: {num_sum}. Total count is: {num_length}. Average for this list is: {average}.")
+ print("Thank you for using average calculator.")
