7251596438d8e9d051a4bf2acbf148709d31d7e7
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-06-05 12:08:22 -0400

    with my_sum function

--- Homework/animal.py
+++ Homework/animal.py
@@ -1,13 +1,11 @@
 def my_sum(numbers):
-    sum = 0
+    total = 0
     for num in numbers:
-        sum += num
-    
-    return sum
+        total += num
+    return total
 
 if __name__ == "__main__":
     user_numbers = []
- 
     print("AVERAGE CALCULATOR")
     while True:
         user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ")
@@ -15,10 +13,8 @@ if __name__ == "__main__":
             break
         else: 
             user_numbers.append(int(user_input))
- 
     num_sum = my_sum(user_numbers)
     num_length = len(user_numbers)
     average = num_sum / num_length
- 
     print(f"Total sum is: {num_sum}. Total count is: {num_length}. Average for this list is: {average}.")
     print("Thank you for using average calculator.")
