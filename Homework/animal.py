519c76087563b8d849ce99de798cb74d1f25e844
ireshalakmali <ireshalakmalibmv@gmail.com>
2024-06-05 12:39:25 -0400

    this is how I am going to start

--- Homework/animal.py
+++ Homework/animal.py
@@ -1,20 +1,15 @@
-def my_sum(numbers):
-    total = 0
-    for num in numbers:
-        total += num
-    return total
 
-if __name__ == "__main__":
-    user_numbers = []
-    print("AVERAGE CALCULATOR")
-    while True:
-        user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ")
-        if user_input == "":
-            break
-        else: 
-            user_numbers.append(int(user_input))
-    num_sum = my_sum(user_numbers)
-    num_length = len(user_numbers)
-    average = num_sum / num_length
-    print(f"Total sum is: {num_sum}. Total count is: {num_length}. Average for this list is: {average}.")
-    print("Thank you for using average calculator.")
+
+animals = ['cat', 'tiger', 'gorilla', 'capybara']
+first_element = animals[0]
+print(f"{first_element} comes before all others.")
+
+last_element = animals[-1]
+print(last_element)
+
+final_element = animals[3]
+print(final_element)
+
+middle_boys = animals[1:-1]
+print(middle_boys)
+
---
+++ Lab_03/lab5b.py
@@ -0,0 +1,3 @@
+animals = ['snake', 'hamster', 'scorpion', 'beaver', 'mosquito', 'camel', 'vulture', 'horse', 'python', 'capybara' ]
+for i in range ([0,9,2]):
+    print(animals(i))
